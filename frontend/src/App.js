9e81b33388a2777cf52f07667add2267442b49d4
Pasindu Ratnayake <pasindu@Pasindus-MacBook-Pro.local>
2024-09-27 12:45:08 +0530

    Header and ExploreMenu components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.js";
+import Home from "./pages/Home/Home.js";
+import Cart from "./pages/Cart/Cart.js";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder.js";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
     </div>
   );
 };
---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-content">
+        <h2>Your cravings, delivered fast!</h2>
+        <p>Tomato is a user-friendly food ordering app that connects you with local restaurants for quick and easy meals. Whether it’s a snack or a full meal, Tomato offers a seamless experience with customizable options and real-time order tracking. Browse, order, and enjoy your favorite dishes—all in a few taps!</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
